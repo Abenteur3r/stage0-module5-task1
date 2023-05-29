0cb9f5aa33c84bcb0e2fd6a8da95dd99eddb1d2f
Ayan <ajan.kohan@gmail.com>
2023-05-29 23:38:59 +0600

    m5 task1

--- src/main/java/com/epam/mjc/stage0/AlgorithmComplexity.java
+++ src/main/java/com/epam/mjc/stage0/AlgorithmComplexity.java
@@ -41,6 +41,6 @@ public class AlgorithmComplexity {
      * Return the complexity of the twoCycleSorting algorithm.
      */
     public Complexity twoCycleSorting() {
-        return null;
+        return Complexity.O_N_2;
     }
 }
